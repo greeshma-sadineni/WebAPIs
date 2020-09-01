b456dc6ca43e69fab82baabf4b2d0b34f9dd7e41
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-09-01 13:25:37 +0200

    CRUD With Action Result is completed

---
+++ WebAPIs/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIs.Models;
+
+namespace WebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public static List<Product> products = new List<Product>()
+        {
+             new Product {
+            Id=100578,
+            Name="Austin and Barbeque AABQ WIFI Food Thermometer",
+            Description="Termometer med wifi for en optimal innertemparature",
+            Price=399
+            },
+             new Product {
+            Id=1009334,
+            Name="Anderson  Electrisk Tandare Ecl1.1",
+            Description="Electric stormasker tandare helt utan gas och bransle",
+            Price=299
+            },
+              new Product {
+            Id=100987,
+            Name="Webber NonSticky Spray",
+            Description="BBQ-oljspray som motverkar att ravaror fasten pa gallret",
+            Price=99
+            },
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return products;
+        }
+        //Add New Product
+        [HttpPost]
+        public ActionResult Post([FromBody]Product product)
+        {
+            if (products.Exists(p => p.Id == product.Id))
+            {
+                return Conflict();
+            }
+            products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, products);
+        }
+
+        //Delete the Product
+        [HttpDelete("{Id}")]
+        public ActionResult<IEnumerable<Product>> Delete(int id)
+        {
+            var product = products.Where(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products = products.Except(product).ToList();
+            return products;
+        }
+        //Update Product
+        [HttpPut("{Id}")]
+        public ActionResult<IEnumerable<Product>> Put(int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            var existingProduct = products.Where(p => p.Id == id);
+            products = products.Except(existingProduct).ToList();
+            products.Add(product);
+            return products;
+        }
+
+    }
+}
--- WebAPIs/Startup.cs
+++ WebAPIs/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,28 +13,27 @@ namespace WebAPIs
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { set; get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
