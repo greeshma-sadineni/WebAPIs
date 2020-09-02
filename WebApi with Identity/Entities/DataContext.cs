1879e8dd0098877af4eb03853ae70cdc7ab1a1e7
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-09-02 16:47:33 +0200

    Working on WebApi with Identity

---
+++ WebApi with Identity/Entities/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_with_Identity.Entities
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApi with Identity/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_with_Identity.Entities
+{
+    public class User
+    {[Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LasttName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public byte[] PassWordHash { get; set; }
+        [Required]
+        public byte[] PassWordSalt { get; set; }
+    }
+}
