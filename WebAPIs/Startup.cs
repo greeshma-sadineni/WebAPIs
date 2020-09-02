a0a470597bf508d04bd0d10004922ea42aff4975
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-09-02 10:41:53 +0200

    JavaScript and Web Api is Working.

--- WebAPIs/Startup.cs
+++ WebAPIs/Startup.cs
@@ -26,7 +26,7 @@ namespace WebAPIs
            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //This method gets called by the runtime.Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors(
